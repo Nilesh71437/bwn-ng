bdb9f02b5b0905aafd089d910d27f5a985349c59
unknown <nilgattani@gmail.com>
2020-02-14 13:46:28 +0530

    Addes Header Components and sccs sytles

---
+++ src/app/comman/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { style } from '@angular/core/src/animation/dsl';
+
+@Component({
+    selector:"bwn-header",
+    templateUrl:"./header.component.html",
+    styleUrls: ["./header.component.scss"]
+})
+export class HeaderComponent
+{
+
+}
